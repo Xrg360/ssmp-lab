7c511864677ed57e8b2c016d5616c11152f9a70d
Adrin7113 <adrinjosect@gmail.com>
2023-12-13 12:29:45 +0530

    Macro Complete (Verified)

--- SS/Macro/macro.c
+++ SS/Macro/macro.c
@@ -3,80 +3,81 @@
 #include <stdlib.h>
 void main()
 {
-    FILE *f1, *f2, *f3, *f4, *f5;
+    FILE *input, *namtab, *deftab, *argtab, *op;
     int len, i, pos = 1;
     char
         arg[20],
-        mne[20], opnd[20], la[20], name[20],
+        opcode[20], operand[20], label[20], name[20],
         mne1[20], opnd1[20], pos1[10], pos2[10];
-    f1 = fopen("input.txt", "r");
-    f2 = fopen("namtab.txt", "w+");
-    f3 = fopen("deftab.txt", "w+");
-    f4 = fopen("argtab.txt", "w+");
-    f5 = fopen("op.txt", "w+");
-    fscanf(f1, "%s%s%s", la, mne, opnd);
-    while (strcmp(mne, "END") != 0)
+    input = fopen("input.txt", "r");
+    namtab = fopen("namtab.txt", "w+");
+    deftab = fopen("deftab.txt", "w+");
+    argtab = fopen("argtab.txt", "w+");
+    op = fopen("op.txt", "w+");
+    fscanf(input, "%s%s%s", label, opcode, operand);
+    while (strcmp(opcode, "END") != 0)
     {
-        if (strcmp(mne, "MACRO") == 0)
+        if (strcmp(opcode, "MACRO") == 0)
         {
-            fprintf(f2, "%s\n", la);
-            fseek(f2, SEEK_SET, 0);
-            fprintf(f3, "%s\t%s\n", la, opnd);
-            fscanf(f1, "%s%s%s", la, mne, opnd);
-            while (strcmp(mne, "MEND") != 0)
+            fprintf(namtab, "%s\n", label);
+            fseek(namtab, SEEK_SET, 0);
+            fprintf(deftab, "%s\t%s\n", label, operand);
+            fscanf(input, "%s%s%s", label, opcode, operand);
+            while (strcmp(opcode, "MEND") != 0)
             {
-                if (opnd[0] == '&')
+                if (operand[0] == '&')
                 {
                     sprintf(pos1, "%d", pos);
                     strcpy(pos2, "?");
-                    strcpy(opnd, strcat(pos2, pos1));
+                    strcpy(operand, strcat(pos2, pos1));
                     pos = pos + 1;
                 }
-                fprintf(f3, "%s\t%s\n", mne, opnd);
-                fscanf(f1, "%s%s%s", la, mne, opnd);
+                fprintf(deftab, "%s\t%s\n", opcode, operand);
+                fscanf(input, "%s%s%s", label, opcode, operand);
             }
-            fprintf(f3, "%s", mne);
+            fprintf(deftab, "%s", opcode);
         }
+        // Macro defenition
         else
         {
-            fscanf(f2, "%s", name);
-            if (strcmp(mne, name) == 0)
+            fscanf(namtab, "%s", name);
+            if (strcmp(opcode, name) == 0)
             {
-                len = strlen(opnd);
+                len = strlen(operand);
                 for (i = 0; i < len; i++)
                 {
-                    if (opnd[i] != ',')
-                        fprintf(f4, "%c", opnd[i]);
+                    if (operand[i] != ',')
+                        fprintf(argtab, "%c", operand[i]);
                     else
-                        fprintf(f4, "\n");
+                        fprintf(argtab, "\n");
                 }
-                fseek(f3, SEEK_SET, 0);
-                fseek(f4, SEEK_SET, 0);
-                fscanf(f3, "%s%s", mne1, opnd1);
-                fprintf(f5, ".\t%s\t%s\n", mne1, opnd);
-                fscanf(f3, "%s%s", mne1, opnd1);
+                fseek(deftab, SEEK_SET, 0);
+                fseek(argtab, SEEK_SET, 0);
+                fscanf(deftab, "%s%s", mne1, opnd1);
+                // fprintf(op, ".\t%s\t%s\n", mne1, operand);
+                fscanf(deftab, "%s%s", mne1, opnd1);
                 while (strcmp(mne1, "MEND") != 0)
                 {
-                    if ((opnd[0] == '?'))
+                    if ((opnd1[0] == '?'))
                     {
-                        fscanf(f4, "%s", arg);
-                        fprintf(f5, "-\t%s\t%s\n", mne1, arg);
+                        fscanf(argtab, "%s", arg);
+                        fprintf(op, "-\t%s\t%s\n", mne1, arg);
                     }
                     else
-                        fprintf(f5, "-\t%s\t%s\n", mne1, opnd1);
-                    fscanf(f3, "%s%s", mne1, opnd1);
+                        fprintf(op, "-\t%s\t%s\n", mne1, opnd1);
+                    fscanf(deftab, "%s%s", mne1, opnd1);
                 }
             }
             else
-                fprintf(f5, "%s\t%s\t%s\n", la, mne, opnd);
+                fprintf(op, "%s\t%s\t%s\n", label, opcode, operand);
         }
-        fscanf(f1, "%s%s%s", la, mne, opnd);
+        fscanf(input, "%s%s%s", label, opcode, operand);
     }
-    fprintf(f5, "%s\t%s\t%s", la, mne, opnd);
-    fclose(f1);
-    fclose(f2);
-    fclose(f3);
-    fclose(f4);
-    fclose(f5);
+    fprintf(op, "%s\t%s\t%s", label, opcode, operand);
+    fclose(input);
+    fclose(namtab);
+    fclose(deftab);
+    fclose(argtab);
+    fclose(op);
     printf("Successfull !!! \n");
 }
