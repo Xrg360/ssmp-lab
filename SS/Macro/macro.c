3e957de744516b66304e9e80acd51d2dfb8e5a3b
Adrin7113 <adrinjosect@gmail.com>
2023-12-13 12:43:02 +0530

    Added Comments to Macro

--- SS/Macro/macro.c
+++ SS/Macro/macro.c
@@ -14,22 +14,29 @@ void main()
     deftab = fopen("deftab.txt", "w+");
     argtab = fopen("argtab.txt", "w+");
     op = fopen("op.txt", "w+");
+
     fscanf(input, "%s%s%s", label, opcode, operand);
     while (strcmp(opcode, "END") != 0)
     {
+        // Macro Defenition
         if (strcmp(opcode, "MACRO") == 0)
         {
             fprintf(namtab, "%s\n", label);
+
+            // Reset position so that we can read from start
             fseek(namtab, SEEK_SET, 0);
+
+            // Macro defenition on the deftab
             fprintf(deftab, "%s\t%s\n", label, operand);
             fscanf(input, "%s%s%s", label, opcode, operand);
             while (strcmp(opcode, "MEND") != 0)
             {
+                // Check if operand is a positional parameter
                 if (operand[0] == '&')
                 {
-                    sprintf(pos1, "%d", pos);
-                    strcpy(pos2, "?");
-                    strcpy(operand, strcat(pos2, pos1));
+                    sprintf(pos1, "%d", pos);            // Converts the integer pos to string pos1. pos1 is "1".
+                    strcpy(pos2, "?");                   // Load "?" to pos2. pos2 is "?".
+                    strcpy(operand, strcat(pos2, pos1)); // We concat ? and 1 to make ?1 and store it in operand.
                     pos = pos + 1;
                 }
                 fprintf(deftab, "%s\t%s\n", opcode, operand);
@@ -37,14 +44,14 @@ void main()
             }
             fprintf(deftab, "%s", opcode);
         }
-        // Macro defenition
+        // OPCODE is not a macro, so write to output file
         else
         {
             fscanf(namtab, "%s", name);
             if (strcmp(opcode, name) == 0)
             {
                 len = strlen(operand);
-                for (i = 0; i < len; i++)
+                for (i = 0; i < len; i++) // Load the arguments to argtab
                 {
                     if (operand[i] != ',')
                         fprintf(argtab, "%c", operand[i]);
@@ -53,12 +60,11 @@ void main()
                 }
                 fseek(deftab, SEEK_SET, 0);
                 fseek(argtab, SEEK_SET, 0);
-                fscanf(deftab, "%s%s", mne1, opnd1);
-                // fprintf(op, ".\t%s\t%s\n", mne1, operand);
-                fscanf(deftab, "%s%s", mne1, opnd1);
+                fscanf(deftab, "%s %s", mne1, opnd1); // Skip the macro invocation line
+                fscanf(deftab, "%s %s", mne1, opnd1);
                 while (strcmp(mne1, "MEND") != 0)
                 {
-                    if ((opnd1[0] == '?'))
+                    if ((opnd1[0] == '?')) // If opnd1 is a positional parameter
                     {
                         fscanf(argtab, "%s", arg);
                         fprintf(op, "-\t%s\t%s\n", mne1, arg);
@@ -69,7 +75,7 @@ void main()
                 }
             }
             else
-                fprintf(op, "%s\t%s\t%s\n", label, opcode, operand);
+                fprintf(op, "%s\t%s\t%s\n", label, opcode, operand); // Write the line as it is to output file. (Normal Case)
         }
         fscanf(input, "%s%s%s", label, opcode, operand);
     }
